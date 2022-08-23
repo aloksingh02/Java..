fe84edf94c32273c79370175b1d3fd69d7d4e5ae
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-08-23 22:41:53 +0530

    found min and max of integer

---
+++ src/com/company/DataSizeRange.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.lang.*;
+
+public class DataSizeRange
+{
+    public static void main(String args[])
+    {
+        System.out.println("Int Min:" +Integer.MIN_VALUE);
+        System.out.println("Int Max:" +Integer.MAX_VALUE);
+        System.out.println("Int Byte:" +Integer.BYTES);
+
+    }
+}
