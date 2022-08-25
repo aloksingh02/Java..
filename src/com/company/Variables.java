f79d8a2b42f8b398dce8ffac1ae7437863d70fbf
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-08-25 23:26:24 +0530

    variable

---
+++ src/com/company/Variables.java
@@ -0,0 +1,17 @@
+package com.company;
+public class Variables {
+    public static void main(String[] args)
+    {
+        byte b=127;
+        short s=30;
+        int i=1500;
+        char c='A';
+        float f=265.656f;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(c);
+        System.out.println(f);
+
+    }
+}
