3073a633145a1ae50160c099b955012d3165f615
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-08-27 23:38:04 +0530

    variable size

---
+++ src/com/company/LITERALS.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.lang.*;
+
+public class LITERALS {
+    public static void main(String arg[])
+    {
+        //byte b=56;
+        //int u= 556;
+        //short f=656;
+        //long d=6565l;
+        //long e =68656L;
+        //long e=6565463554535l;
+        //float f=19.56f;
+        //double d=12.56;
+        byte b1=10;
+        byte b2=0b1010;
+        byte b3=012;
+        byte b4=0XA;
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+    }
+}
--- src/com/company/Variables.java
+++ src/com/company/Variables.java
@@ -2,16 +2,11 @@ package com.company;
 public class Variables {
     public static void main(String[] args)
     {
-        byte b=127;
-        short s=30;
-        int i=1500;
-        char c='A';
-        float f=265.656f;
-        System.out.println(b);
-        System.out.println(s);
-        System.out.println(i);
-        System.out.println(c);
-        System.out.println(f);
+        int x =95;
+        int X;
+        int roll_number_student;
+        int D;
+        System.out.println(x);
 
     }
 }
