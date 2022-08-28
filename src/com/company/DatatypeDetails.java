544990ac27b03243c38c71d4e7a8797630009820
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-08-28 12:23:25 +0530

    variable size

---
+++ src/com/company/DatatypeDetails.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class DatatypeDetails {
+    {
+        int a = 6;
+        int b=56;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toHexString(b));
+    }
+}
