c5d2d445c217cad1bc57d575c85fd33251f47d6a
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-08-31 01:25:26 +0530

    Basic arithmatic representation

---
+++ src/com/company/ARITHMATIC.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class ARITHMATIC {
+    public static void main(String[] args)
+    {
+        //1.Here if we want int we simply give int and if we want out as in decimals we use float as given below
+        //int a=18, b=5;
+        //float c=(float)a/b;
+        //int r=a%b;
+        //System.out.println(c);
+        //System.out.println(r);
+
+        //2.only for float value
+        //float a=15.65f,b=2.54f;
+        //float v=a/b;
+        //float d=a%b;
+        //System.out.println(v);
+        //System.out.println(d);
+
+        //3.now for the expression
+        //byte a=10;
+        //short b=65;
+        //here note that byte+short=int so result come in form of int
+        //int c=a+b;
+        //System.out.println(c);
+
+        //4.long+float=float
+        //int+float=float
+        //float+double=double
+        //long+double=double
+        //byte+short=int
+        //char'+','-'int=int
+
+        //System.out.println(10+20/2);
+        //According to our study til now what we expect the answer is 15, but answer is 20 let check it out
+        //this all we get by not apply the rules of expression now apply it
+        System.out.println((10+20)/2);
+
+    }
+}
