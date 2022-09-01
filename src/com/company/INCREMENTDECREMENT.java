0354abd6c1436a354b3bd1ab1d83cd91683298f6
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-01 12:23:53 +0530

    Increment and decrement on various datatype

---
+++ src/com/company/INCREMENTDECREMENT.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class INCREMENTDECREMENT {
+    public static void main(String[] args){
+        //int x=5 ,y;
+        //y=x++;
+        //System.out.println(y);
+        //System.out.println(x);
+
+        //int a=6 ,b=9 , x=4,c;
+        // c=a * x++ +b;
+        //System.out.println(c);
+
+        //for float
+        //float x=5.3f ,y;
+        //y=x++;
+        //System.out.println(y);
+        //System.out.println(x);
+
+        //for char
+        //char x='A' ,y;
+        //y=x++;
+        //System.out.println(y);
+        //System.out.println(x);
+
+        //for byte
+        //byte b=5 ;
+        //b=b+1;
+        //it never give any output bcoz byte + int never gives output
+
+        //int x=5, y=4, z;
+        //z=2* x+++3*++y;
+        //System.out.println(z);
+
+        //decrement of int
+        //int x=6, c;
+        //c=--x;
+        //System.out.println(c);
+
+       // int x=9, y;
+        //y=x--;
+        //System.out.println(y);
+        //System.out.println(x);
+
+        int a=6, b=4, c=8,d;
+        d=a*--c*b;
+        System.out.println(d);
+    }
+}
