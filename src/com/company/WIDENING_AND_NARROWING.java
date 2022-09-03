859c646464770650d86b6eb0751ff666d3184ecd
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-03 16:40:09 +0530

    Widening and narrowing the data

---
+++ src/com/company/WIDENING_AND_NARROWING.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class WIDENING_AND_NARROWING {
+    public static void main(String[] args)
+    {
+        byte b=10;
+        short s=5;
+        int i=10;
+        double d=10;
+        float f=10;
+        long l=10;
+        char c=10;
+        boolean bl=true;
+
+       //s=b;
+       //i=b;
+       //l=b;
+       //f=b;
+       //l=b;
+       //character not assign byte
+
+        b=(byte)s;//it cannot assign if and only if it follow the narrowing
+        i=s;
+        l=s;
+        f=s;
+        d=s;
+        System.out.println(b);
+
+
+
+
+    }
+}
