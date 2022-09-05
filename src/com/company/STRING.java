a6f6bc8564dfdc7be3c11d1af18df2a1cec5ef9e
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-05 19:21:47 +0530

    basic codes of string

--- src/com/company/STRING.java
+++ src/com/company/STRING.java
@@ -3,6 +3,26 @@ package com.company;
 public class STRING {
     public static void main(String[] args)
     {
+        //Here there are some multiple ways to representation of string
+        //String str1 = "java program";
+        //String str2= "alok kumar singh";
 
+        //char c[]={'j','a','v','a','p','r','o','g','r','a','m'};
+        //String str3 = new String(c,3,4);
+
+        //byte b[]={68,67,66,65};
+        //String str4 = new String(b,1,2);
+        //now in this if we want to print only 66,67 then in little change we have to do in new section as same we are allowed to do different line also
+        //System.out.println(str3);
+
+        //checking whether these two reference are equal or not and answer we got in true and false
+
+        //String str1 = "java";
+        //String str2 = "java";
+        //System.out.println(str1==str2);
+
+        String str1 = "Java";
+        String str2 = "java";
+        System.out.println(str1==str2);
     }
 }
