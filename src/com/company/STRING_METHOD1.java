bb00c2b1f50494594b35fd086f2c70232cfeab83
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-06 19:06:35 +0530

    method3 of string for more use

---
+++ src/com/company/STRING_METHOD1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class STRING_METHOD1 {
+    public static void main(String[] args)
+    {
+        //String str1="pyramid";
+        //String str2="pyramid";
+        //System.out.println(str1.equals(str2));
+        //System.out.println(str1==str2);
+        //System.out.println(str1.equals(str2));
+
+        //System.out.println(str1.compareTo(str2));
+
+        String str1 = "zlok";
+        String str2 = "aLOK";
+        System.out.println(str1.compareTo(str2));
+
+    }
+}
