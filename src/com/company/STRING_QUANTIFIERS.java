f0ee92c1a6524127ba7a7a811c8ec78ac8d95733
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-07 17:36:39 +0530

    STRING_QUANTIFIER

---
+++ src/com/company/STRING_QUANTIFIERS.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class STRING_QUANTIFIERS {
+    public static void main(String[] args) {
+        // REGULAR EXPRESSION         DESCRIPTION                    Example
+        //       .,*                 0 or more time            we can write [abc]*  here letter must be abc but we can write many times
+        //        +                  one or more time
+        //        ?                  0 or 1 time
+        //       {X}                  x times
+        //       {X,Y}              Between x and y time
+
+
+         //String str1 = "abc65def";
+         //System.out.println(str1.matches("[a-z]*"));
+         //WE are using this for applicable for many times .which quantifier generally do in above code in put we can even not give input
+
+        //String str1 = "acc";
+        //System.out.println(str1.matches("[abc]+"));
+
+        //String str2 = "accb";
+        //System.out.println(str2.matches("[abc]{3,7}"));
+        //3 mean exact 3 count must there if we give 3,7 which mean it is upto 7
+
+        //for email
+        String str1 = "john@gmail.com";
+        System.out.println(str1.matches("\\w*@gmail(.*)"));
+
+
+
+    }
+}
