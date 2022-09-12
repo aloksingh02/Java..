a3d455db64b0d1ffe9afc9afd6402254d9cb03e1
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-12 17:22:15 +0530

    WHILE LOOP PRACTICE

---
+++ src/com/company/LOOP.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class LOOP {
+    public static void main(String[] args) {
+        int i= 1;
+
+       // while(i<100)
+       // {
+       //     System.out.println(i);
+       //     i=i*2;
+       // }
+        // FOR DO-WHILE LOOP
+
+        //do
+        //{
+        //    System.out.println(i);
+        //    i=i*2;
+        //}while(i<1000);
+
+        //For infinite loop
+        while(true)
+        {
+            System.out.println(i);
+            i++;
+            //here we can also replace the int with short by which loop which is created is in the range
+            //we can stop it by using S.O.P("BYE")
+        }
+
+    }
+}
