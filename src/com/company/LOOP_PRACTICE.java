2291d73e294d57fbc13bf1662de374a53f30f017
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-13 16:24:12 +0530

    WHILE LOOP PRACTICE1

---
+++ src/com/company/LOOP_PRACTICE.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+
+public class LOOP_PRACTICE {
+    public static void main(String[] args) {
+       //REMEMBER ONE THING THAT IF IN ANY QUESTION IT ASKED TO CREATE ENVIRONMENT IT SIMPE MEANS FOR TO IMPORT SCANNER
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        //for(int i=1; i<=10; i++)
+        //{
+        //    System.out.println(n+"x"+i+"="+n*i);
+       // }
+
+
+        //SUM OF THE first n NUMBER
+        int sum=0;
+        for(int i=1;  i<=n; i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println("Sum of "+n+" Number is "+sum);
+
+
+    }
+}
