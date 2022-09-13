b66481b763da6050c7081842b2741d448f493a24
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-13 18:54:29 +0530

    nested_loop

---
+++ src/com/company/NESTED_LOOP.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class NESTED_LOOP {
+    public static void main(String[] args) {
+        //nested loop
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.println("("+i+","+j+") ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
