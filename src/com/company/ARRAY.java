a9c4e6bba11a629325b5249a0a4deb58d8a976ae
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-09-16 13:48:24 +0530

    ARRAY

--- src/com/company/ARRAY.java
+++ src/com/company/ARRAY.java
@@ -2,7 +2,16 @@ package com.company;
 
 public class ARRAY {
     public static void main(String[] args) {
+         //WAY OF DECLARATION OF ARRAY
+         int A[] = new int[10];
+         int B[] = {1,2,3,4,5};
+         int C[];
+         C=new int [10];
 
-
+         B[2]=15;
+         for(int i=0; i<B.length;i++)
+         {
+             System.out.println(B[i]);
+         }
     }
 }
